157a5afd62688db5bccb321f256653d52026520a
Kona Marie <ambermoreyra@gmail.com>
2019-05-17 16:46:15 -0600

    html and initial css complete

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+//create start button that triggers start of timer
+//user chooses answers to ten questions
+//user clicks submit
+//correct and incorrect answers are calculated and displayed
+//if timer = 0, the game ends and calculates/displays # correct
